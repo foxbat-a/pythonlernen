61b339f25eff59d0c1789bb61b423a59fe8ccb65
aa43252 <aa43252@FFTGLMDV0000143.eur.nsroot.net>
2014-01-29 11:38:49 +0400

    Die dritte Aufgabe

--- src/main.py
+++ src/main.py
@@ -1,11 +1,26 @@
-# Aufgabe Nummer 2
+print "I will now count my chickens:"
 
-# A comment, this is so you can read your program later.
-# Anything after the # is ignored by Python
+# 25 plus 5 ist: 30
+print "Hens: ", 25 + 30 / 6
+# 25 mal 3 ist 75; 75 mod 4 ist 3; 100 minus 3 ist 97
+print "Roosters: ", 100 - 25 * 3 % 4
 
-print "I could have code like this" # and the comment after is ignored
+print "Now I will count the eggs:"
 
-# You can also use a comment to disable or comment out a piece of code:
-# print "This won't run"
+#Fuck! 4%2 ist 0
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
-print "This will run."
+print "I it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more?"
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
