65968c2a7a08dd784972c08db4bb0801ac195530
aa43252 <aa43252@FFTGLMDV0000143.eur.nsroot.net>
2014-01-30 12:40:34 +0400

    Die fuenfte Aufgabe mit покращеннями

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+C_KILOS_IN_POUND = 0.45359237
+C_CMS_IN_FOOT = 30.48
+
 my_name = 'Alex P. Art'
 my_age = 30 # keine Luege
 my_height = 188
@@ -6,9 +9,13 @@ my_eyes = 'Blue'
 my_teeth = 'White'
 my_hair = 'Schwarz'
 
+my_weight_pounds = my_weight / C_KILOS_IN_POUND
+my_height_feet = my_height / C_CMS_IN_FOOT
+
+
 print "Let's talk about %s" % my_name
-print "He's %s cm tall" % my_height
-print "He's %s kilo heavy" % my_weight
+print "He's %s cm (%s feet) tall" % (my_height, my_height_feet)
+print "He's %s kilo (%s pounds) heavy" % (my_weight, my_weight_pounds)
 print "Actually, that's not too heavy."
 print "he's got %s eyes and %s hair." % (my_eyes, my_hair)
 print "His teeth are usually %s, depending on coffee" % my_teeth
