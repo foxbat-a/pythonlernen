eab5802416e0fa5e9f17e25fe389450aae6a33b7
aa43252 <aa43252@FFTGLMDV0000143.eur.nsroot.net>
2014-01-31 19:30:17 +0400

    Die sechste Aufgabe

--- src/main.py
+++ src/main.py
@@ -1,25 +1,20 @@
-C_KILOS_IN_POUND = 0.45359237
-C_CMS_IN_FOOT = 30.48
+x = "There are %d types of people." % 10
+binary = "binary"
+do_not = "don't"
+y = "Those who know %s and those who %s."  % (binary, do_not)
 
-my_name = 'Alex P. Art'
-my_age = 30 # keine Luege
-my_height = 188
-my_weight = 88
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Schwarz'
+print x
+print y
 
-my_weight_pounds = my_weight / C_KILOS_IN_POUND
-my_height_feet = my_height / C_CMS_IN_FOOT
+print "I said: %r." % x
+print "I also said: '%s'." % y
 
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! %r"
 
-print "Let's talk about %s" % my_name
-print "He's %s cm (%s feet) tall" % (my_height, my_height_feet)
-print "He's %s kilo (%s pounds) heavy" % (my_weight, my_weight_pounds)
-print "Actually, that's not too heavy."
-print "he's got %s eyes and %s hair." % (my_eyes, my_hair)
-print "His teeth are usually %s, depending on coffee" % my_teeth
+print joke_evaluation % hilarious
 
-# and now... 
-print "If I add %d, %d and %d, I get %d" % (
-        my_age, my_height, my_weight, my_age + my_height + my_weight)
+w = "This is the left side of..."
+e = "a string with a right side."
+
+print w + e
