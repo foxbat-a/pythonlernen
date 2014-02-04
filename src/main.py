10db1e45cc3d940dd45b221e16362ffdad4943e6
aa43252 <aa43252@FFTGLMDV0000143.eur.nsroot.net>
2014-02-04 18:15:26 +0400

    Die achteste Aufgabe

--- src/main.py
+++ src/main.py
@@ -1,21 +1,12 @@
-print "Mary had a little lamb."
-print "Its fleece was white as %s." % 'snow'
-print "And everywhere that Mary went."
-print ("-" + ".") * 10 #what?!
+formatieur = "%r %r %r %r"
 
-end1 = "C"
-end2 = "h"
-end3 = "e"
-end4 = "e"
-end5 = "s"
-end6 = "e"
-end7 = "B"
-end8 = "u"
-end9 = "r"
-end10 = "g"
-end11 = "e"
-end12 = "r"
-
-#follow the comma, motherfucker!
-print end1 + end2 + end3 + end4 + end5 + end6
-print end7 + end8 + end9 + end10 + end11 + end12
+print formatieur % (1, 2, 3, 4)
+print formatieur % ("one", "two", "three", "four")
+print formatieur % (True, False, False, True)
+print formatieur % (formatieur, formatieur, formatieur, formatieur)
+print formatieur % (
+    "I had this thing.",
+    "That you could type up right.",
+    "But it didn't sing.",
+    "So I said goodnight."
+)
