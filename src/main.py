ced61875d125a69de4aceb52d5431f8112bc9188
aa43252 <aa43252@FFTGLMDV0000143.eur.nsroot.net>
2014-02-26 18:27:56 +0400

    Die dreizehnste Aufgabe - ein bisschen besser

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
 from sys import argv
 
-script, erste, zweite, dritte = argv
+tmp_array=argv
+if len(argv) < 4:
+    for i in range(len(argv), 4):
+        tmp_array.append(raw_input("Enter argument #%d" % i))
+elif len(argv) > 4:
+    tmp_array=tmp_array[0:4]
+    
+script, erste, zweite, dritte = tmp_array
 
 print "Das skript wird bearbeitet: ", script
 print "Ihre erste Variable: ", erste
