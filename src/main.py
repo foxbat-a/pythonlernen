4eae5818847d1485f2ef9f60655f813a83dd3dd6
aa43252 <aa43252@FFTGLMDV0000143.eur.nsroot.net>
2014-02-26 17:49:44 +0400

    Die elfste Aufgabe

--- src/main.py
+++ src/main.py
@@ -1,23 +1,6 @@
-tabby_cat = "\tI'm tabbed in."
-persian_cat = "I'm split\non a line."
-backslash_cat = "I'm \\ a \\ cat."
+age = raw_input("Wie alt sind Sie? ")
+height = raw_input("Wie hoch sind Sie? ")
+weight = raw_input("Wie wiegen Sie? ")
 
-fat_cat = '''
-I\'ll do a list:
-\t* Cat food
-\t* Fischies
-\t* Catnip\n\t* Grass
-'''
-
-print tabby_cat
-print persian_cat
-print backslash_cat
-print fat_cat
-
-v_cat = '"\'cat"'
-v_dog="Le'\"dog"
-
-print 'Imma %s %r' % (v_cat, v_cat)
-print 'Imma %s %r' % (v_dog, v_dog)
-print "Imma %s %r" % (v_cat, v_cat)
-print "Imma %s %r" % (v_dog, v_dog)
+print "Sie sind %r Jahre alt, Sie sind %r hoch und Sie wiegen %r" % (
+age, height, weight)
