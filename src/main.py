815d21d36b85b7ea0915e34567f03b5b5a2f1ec2
aa43252 <aa43252@FFTGLMDV0000143.eur.nsroot.net>
2014-03-12 15:55:44 +0400

    Die fuenfzehnste Aufgabe

--- src/main.py
+++ src/main.py
@@ -1,21 +1,15 @@
 from sys import argv
 
-script, user_name = argv
-prompt = '> '
+script, filename = argv
 
-print "Hi, %s, I'm %s script." % (user_name, script)
-print "I'd like to ask you a few questions."
-print "Do you like me %s?" % user_name
-likes = raw_input(prompt)
+txt = open(filename)
 
-print "Where do you live %s?" % user_name
-lives = raw_input(prompt)
+print "Da ist Ihre Datei, %r:" % filename
+print txt.read()
 
-print "What kind of computer do you have?"
-computer = raw_input(prompt)
+print "Eingeben Sie den Dateinamen noch einmal: "
+file_again = raw_input("> ")
 
-print """
-Na, gut, Sie hat gesagt dass Sie %r moegen mir.
-Sie wohnen in %r. Not sure where that is.
-Und Sie haben der %r computer. Wunderbar!
-""" % (likes, lives, computer)
+txt_again = open(file_again)
+
+print txt_again.read()
