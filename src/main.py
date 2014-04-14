960d4fe7cd09f91b9ee44c374f359ac4d9dcb004
AA43252 <AA43252@FFTGLMDV0000143.eur.nsroot.net>
2014-04-14 13:40:43 +0400

    Die neunzehnste Aufgabe - Deutlich

--- src/main.py
+++ src/main.py
@@ -1,17 +1,20 @@
-def print_two(*args):
-    arg1, arg2 = args
-    print "arg1: %r, arg2: %r" % (arg1, arg2)
+def cheese_and_crackers(cheese_count, boxes_of_crackers):
+    print "Sie haben %d Stuecke Kaese!" % cheese_count
+    print "Sie haben %d Kaesten Gebaeck!" % boxes_of_crackers
+    print "Es ist genug, um Party zu machen!"
+    print "Das machen wir!\n"
     
-def print_two_again(arg1, arg2):
-    print "arg1: %r, arg2: %r" % (arg1, arg2)
-    
-def print_one(arg1):
-    print "arg1: %r" % arg1
-    
-def print_none():
-    print "I got nothing'."
-    
-print_two("Zed", "Shaw")
-print_two_again("Zed", "Shaw")
-print_one("First!")
-print_none()
+print "We can just give the function numbers directly:"
+cheese_and_crackers(20, 30)
+
+print "Or we can use variables from script:"
+amt_cheese = 10
+amt_crackers = 50
+
+cheese_and_crackers(amt_cheese, amt_crackers)
+
+print "We can even do math inside too:"
+cheese_and_crackers(10 + 20, 5 + 6)
+
+print "And we can combine the two, variables and math:"
+cheese_and_crackers(amt_cheese + 100, amt_crackers + 1000)
