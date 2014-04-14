4f9a8245a24b479ab22e79ac6df5464c966874ea
AA43252 <AA43252@FFTGLMDV0000143.eur.nsroot.net>
2014-04-14 14:15:39 +0400

    Die neunzehnste Aufgabe -Deutsch

--- src/main.py
+++ src/main.py
@@ -4,17 +4,17 @@ def cheese_and_crackers(cheese_count, boxes_of_crackers):
     print "Es ist genug, um Party zu machen!"
     print "Das machen wir!\n"
     
-print "We can just give the function numbers directly:"
+print "Zuerst, koennen wir Zahlen benutzen:"
 cheese_and_crackers(20, 30)
 
-print "Or we can use variables from script:"
+print "Dann koennen wir Variable von Skript verwenden:"
 amt_cheese = 10
 amt_crackers = 50
 
 cheese_and_crackers(amt_cheese, amt_crackers)
 
-print "We can even do math inside too:"
+print "Dann koennen wir ein bisschen Mathe verwenden:"
 cheese_and_crackers(10 + 20, 5 + 6)
 
-print "And we can combine the two, variables and math:"
+print "Endlich, koennen wir Mathe und Variable mischen:"
 cheese_and_crackers(amt_cheese + 100, amt_crackers + 1000)
